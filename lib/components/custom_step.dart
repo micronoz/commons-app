4ade94e77284e52d61ef69fbf5d59c7b9fc6a2ca
Nabi Ozberkman <n.ozberkman@gmail.com>
2020-12-31 00:23:24 -0800

    Making create tribe page

---
+++ lib/components/custom_step.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class CustomStep extends Step {
+  /// Creates a step for a [Stepper].
+  ///
+  /// The [title], [content], and [state] arguments must not be null.
+  CustomStep(
+      {@required this.title,
+      this.subtitle,
+      @required content,
+      this.state = StepState.indexed,
+      this.isActive = false,
+      bool form = false,
+      this.formKey})
+      : assert(title != null),
+        assert(content != null),
+        assert(state != null),
+        super(
+            title: title,
+            subtitle: subtitle,
+            content: content,
+            state: state,
+            isActive: isActive) {
+    if (form) {
+      print('Assigning key');
+      this.content = Form(
+        key: formKey,
+        child: content,
+      );
+    } else
+      this.content = content;
+  }
+
+  /// The title of the step that typically describes it.
+  @override
+  Widget title;
+
+  /// The subtitle of the step that appears below the title and has a smaller
+  /// font size. It typically gives more details that complement the title.
+  ///
+  /// If null, the subtitle is not shown.
+  @override
+  Widget subtitle;
+
+  /// The content of the step that appears below the [title] and [subtitle].
+  ///
+  /// Below the content, every step has a 'continue' and 'cancel' button.
+  @override
+  Widget content;
+
+  /// The state of the step which determines the styling of its components
+  /// and whether steps are interactive.
+  @override
+  StepState state;
+
+  /// Whether or not the step is active. The flag only influences styling.
+  @override
+  bool isActive;
+
+  GlobalKey<FormState> formKey;
+
+  bool validate() {
+    assert(this.formKey != null);
+    return formKey.currentState.validate();
+  }
+
+  void save() {
+    assert(this.formKey != null);
+    formKey.currentState.save();
+  }
+}
+
+class CustomStepState {
+  CustomStepState({
+    this.state = StepState.indexed,
+    this.isActive = false,
+  }) : assert(state != null) {
+    formKey = GlobalKey<FormState>();
+    key = UniqueKey();
+  }
+
+  /// The state of the step which determines the styling of its components
+  /// and whether steps are interactive.
+  StepState state;
+
+  /// Whether or not the step is active. The flag only influences styling.
+  bool isActive;
+
+  GlobalKey<FormState> formKey;
+
+  Key key;
+}
--- lib/pages/create_tribe.dart
+++ lib/pages/create_tribe.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tribal_instinct/components/custom_step.dart';
 
 class CreateTribePage extends StatefulWidget {
   @override
@@ -6,32 +7,140 @@ class CreateTribePage extends StatefulWidget {
 }
 
 class _CreateTribePageState extends State<CreateTribePage> {
+  bool cancelling = false;
   int currentStep = 0;
-  goTo(int step) {
-    setState(() => currentStep = step);
+  List<Step> steps;
+  List<CustomStepState> stepStates = [CustomStepState(isActive: true)] +
+      [1].map((e) => CustomStepState()).toList();
+  bool complete = false;
+  final formSpacing = const SizedBox(
+    height: 20,
+  );
+
+  String tribeName;
+  String tribeMotto;
+  String tribeSummary;
+
+  void next() {
+    print('Next called');
+    currentStep + 1 != steps.length
+        ? goTo(currentStep + 1)
+        : setState(() => complete = true);
   }
 
+  void cancel() {
+    setState(() {
+      cancelling = true;
+    });
+  }
+
+  void goTo(int step) {
+    print('Goto called');
+    var chooseStep = (steps[currentStep] as CustomStep);
+    var stepState = stepStates[currentStep];
+    setState(() {
+      if (!chooseStep.validate()) {
+        stepState.state = StepState.error;
+      } else {
+        chooseStep.save();
+        stepState.isActive = false;
+        stepState.state = StepState.complete;
+        currentStep = step;
+        stepStates[currentStep].isActive = true;
+      }
+    });
+  }
+
+  //TODO Move customstep content rendering to build method and map content to customstep
   @override
   Widget build(BuildContext context) {
+    steps = [
+      CustomStep(
+        isActive: stepStates[0].isActive,
+        state: stepStates[0].state,
+        form: true,
+        formKey: stepStates[0].formKey,
+        content: Column(
+          key: stepStates[0].key,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Name your Tribe',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            TextFormField(
+              initialValue: tribeName,
+              validator: (value) => value.isEmpty ? '*Required' : null,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              onSaved: (newValue) => tribeName = newValue,
+              onChanged: (newValue) => tribeName = newValue,
+            ),
+            formSpacing,
+            Text(
+              'State your Tribe\'s motto',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            TextFormField(
+              initialValue: tribeMotto,
+              validator: (value) => value.isEmpty ? '*Required' : null,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              onSaved: (newValue) => tribeMotto = newValue,
+              onChanged: (newValue) => tribeMotto = newValue,
+            ),
+            formSpacing,
+            Text(
+              'Describe your Tribe',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            TextFormField(
+              initialValue: tribeSummary,
+              validator: (value) => value.isEmpty ? '*Required' : null,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              onSaved: (newValue) => tribeSummary = newValue,
+              onChanged: (newValue) => tribeSummary = newValue,
+            ),
+            formSpacing,
+            Text(
+              '*Note: All of the above can be changed in the future',
+              style: Theme.of(context).textTheme.bodyText2,
+            ),
+          ],
+        ),
+        title: Text('Genesis'),
+      ),
+      CustomStep(
+        form: true,
+        formKey: stepStates[1].formKey,
+        state: stepStates[1].state,
+        isActive: stepStates[1].isActive,
+        content: Text('Name your Tribe'),
+        title: Text('Overall'),
+      )
+    ];
     return Scaffold(
       appBar: AppBar(
         title: Text('Create a Tribe'),
       ),
-      body: Stepper(
-        type: StepperType.horizontal,
-        steps: [
-          Step(
-            state: StepState.complete,
-            isActive: true,
-            content: Text('Name your Tribe (This cannot be changed!)'),
-            title: Text('Name'),
-          ),
-          Step(
-            content: Text('Name your Tribe (This cannot be changed!)'),
-            title: Text('Name'),
-          )
-        ],
-      ),
+      body: cancelling
+          ? AlertDialog(
+              title: Text('Are you sure you want to cancel?'),
+              actions: [
+                TextButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: Text('Yes')),
+                TextButton(
+                    onPressed: () => setState(() => cancelling = false),
+                    child: Text('No'))
+              ],
+            )
+          : Stepper(
+              currentStep: currentStep,
+              onStepTapped: goTo,
+              onStepContinue: next,
+              onStepCancel: cancel,
+              type: StepperType.horizontal,
+              steps: steps,
+            ),
     );
   }
 }
