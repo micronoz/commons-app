5d78b7e01756df156a71ddb36c5adffaca707464
Nabi Ozberkman <n.ozberkman@gmail.com>
2021-02-07 23:00:16 -0800

    Add title

--- lib/pages/clubs/clubs.dart
+++ lib/pages/clubs/clubs.dart
@@ -12,6 +12,7 @@ class _ClubsPageState extends State<ClubsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text('Clubs'),
         actions: [
           IconButton(
               onPressed: () => Navigator.of(context).push(
