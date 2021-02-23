b9a263d022eafe2a93584d63e0ea510761b3379d
alermi <alermi@ucsd.edu>
2021-02-23 14:37:54 -0800

    Simplified discovery page filtering

--- lib/pages/discover/discover_category.dart
+++ lib/pages/discover/discover_category.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:tribal_instinct/components/event_card_small.dart';
 import 'package:tribal_instinct/model/activity.dart';
 import 'package:tribal_instinct/model/discover_types.dart';
-import 'package:tribal_instinct/pages/discover/discover_items.dart';
 
 class DiscoverCategoryPage extends StatefulWidget {
   final DiscoverType type;
@@ -13,65 +12,61 @@ class DiscoverCategoryPage extends StatefulWidget {
 }
 
 class _DiscoverCategoryPageState extends State<DiscoverCategoryPage> {
-  final List<Map<String, Object>> _categories = [
-    {'name': 'Sports', 'icon': Icon(Icons.sports_football)},
-    {'name': 'Books', 'icon': Icon(Icons.book)}
+  final List<String> _cast = <String>[
+    'All',
+    'Hang Out',
+    'Study',
+    'Sports',
+    'Other',
   ];
+  final List<String> _filters = <String>[];
+
+  Iterable<Widget> get categoryWidgets sync* {
+    for (final category in _cast) {
+      yield Padding(
+        padding: const EdgeInsets.all(1.0),
+        child: FilterChip(
+          label: Text(category),
+          selected: category == 'All'
+              ? _filters.isEmpty
+              : _filters.contains(category),
+          onSelected: (bool value) {
+            setState(() {
+              if (value) {
+                if (category == 'All') {
+                  _filters.clear();
+                } else {
+                  _filters.add(category);
+                }
+              } else {
+                if (category != 'All') {
+                  _filters.removeWhere((String name) {
+                    return name == category;
+                  });
+                }
+              }
+            });
+          },
+        ),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    var tiles = _categories
-        .map(
-          (e) => ListTile(
-            key: Key(e['name']),
-            onTap: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) =>
-                      DiscoverItemsPage(widget.type, e['name'])));
-            },
-            tileColor: Colors.brown[100],
-            leading: e['icon'],
-            title: Text(e['name']),
-            trailing: Icon(
-              Icons.arrow_right,
-              size: 30,
-            ),
-          ),
-        )
-        .toList();
-    tiles.sort((a, b) => a.key.toString().compareTo(b.key.toString()));
-
-    var categories = ListTile.divideTiles(
-      color: Colors.black,
-      context: context,
-      tiles: tiles,
-    ).toList();
-
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ListView(
         children: <Widget>[
-              Text(
-                'Your friends are attending',
-                style: Theme.of(context).textTheme.headline2,
-                textScaleFactor: 0.4,
-              ),
-              EventCardSmall(Activity.getDefault()),
-              EventCardSmall(Activity.getDefault()),
-              EventCardSmall(Activity.getDefault()),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                'Discover categories',
-                style: Theme.of(context).textTheme.headline2,
-                textScaleFactor: 0.4,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-            ] +
-            categories,
+          Wrap(
+            direction: Axis.horizontal,
+            alignment: WrapAlignment.spaceEvenly,
+            children: categoryWidgets.toList(),
+          ),
+          EventCardSmall(Activity.getDefault()),
+          EventCardSmall(Activity.getDefault()),
+          EventCardSmall(Activity.getDefault()),
+        ],
       ),
     );
   }
